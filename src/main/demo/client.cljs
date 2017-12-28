500fac1508bbf002a6cc72ce128a6029fac9d2d2
Andrea Lai <laidevpub@gmail.com>
2017-12-28 13:29:20 +0100

    loading data from server - is not working

--- src/main/demo/client.cljs
+++ src/main/demo/client.cljs
@@ -1,4 +1,11 @@
 (ns demo.client
-  (:require [fulcro.client :as fc]))
+  (:require [fulcro.client :as fc]
+            [fulcro.client.data-fetch :as df]               ; (1)
+            [demo.ui.geoppr :as root]))
 
-(defonce app (atom (fc/new-fulcro-client)))
+(defonce app (atom (fc/new-fulcro-client
+                     :started-callback
+                     (fn [app]                              ; (2)
+                       (df/load app :geoppr/test root/Root))
+
+                     :initial-state {:geoppr/inizio 42})))
